a8d1ca4bd2ced40e7672adc37defd84c1af27083
Sanket8090 <gulhanesanket007@gmail.com>
2024-02-17 23:19:24 +0530

    Solve

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+// import Timer from './Timer'
+import Timer from "./component/Timer" 
+import './App.css'
+function App() {
+  const [show, setShow] = useState(false)
+  return (
+    <>
+    <button onClick={()=>setShow(!show)}>Toogel</button>
+    {show ?<Timer/>:<p>please toogel the switch to get timer</p> }
+    </>
+  )
+}
+export default App
